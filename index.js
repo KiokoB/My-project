ff3bf1cc0d7b78ae913e17ac337ceb297e446046
Bianca Kioko <kiokobianca01@gmail.com>
2024-04-13 11:37:24 +0300

    updated patch-request

--- index.js
+++ index.js
@@ -10,15 +10,24 @@ document.addEventListener('DOMContentLoaded', ()=>{
             <img src="${phone.img}" class="card-img-top" alt="...">  
             <div class="card-body">
               <h5 class="card-title">${phone.name}</h5>
+              <p>
+              <span class="stock-value">${phone.quantity}</span></p>
               <p class="card-text">${phone.description}</p>
               <button id="purchase" class="btn btn-primary">${phone.price}</button>
               <button id="delete">Sold out</button>
             </div>
           </div>
         `
-        div.querySelector('#purchase').addEventListener('click', ()=>{
-            console.log('clicked')
+        const buy =div.querySelector('#purchase')
+        buy.addEventListener('click', ()=>{
+           // phone.quantity = 1
+            //div.querySelector('span').textContent = phone.quantity - 1;
+            const remainingStock =(phone.quantity - 1)
+            div.querySelector('span').textContent = remainingStock;
+            updateData(phone)
         });
+
+        //Delete an element 
         div.querySelector('#delete').addEventListener('click', ()=>{
             div.remove()
            deletePhone(phone.id) 
@@ -43,24 +52,24 @@ function getPhones(){
     getPhones()
 
 //Make a patch request
-// function updateData(){
-//     fetch(baseUrl, {
-//         method: 'PATCH',
-//         headers: {
-//             'Content-Type': 'application/json' 
-//         },
-//         body:JSON.stringify()
-//     })
-//     .then(response=>response.json())
-//     .then(data=>data)
-// }
+function updateData(phone){
+    fetch(`http://localhost:3000/phones/${phone.id}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json' 
+        },
+        body:JSON.stringify(phone)
+    })
+    .then(response=>response.json())
+    .then(phone=> console.log(phone))
+}
 
 
 
 
 //Delete an item once its out of stock   
 function deletePhone(id){
-    fetch(`http://localhost:3000/phones/${id}`, {
+    fetch(`http://localhost:3000/phones/${id}`,{
         method: 'DELETE',
         headers: {
             'Content-Type': 'application/json'
